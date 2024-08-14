0ece880afa75092f75c89510a55fad9e35b1bb07
Rycia Soulblight <43384589+Rycia@users.noreply.github.com>
2024-08-14 01:08:36 -0500

    Initial commit

---
+++ BetterBags_Gearsets/main.lua
@@ -0,0 +1,43 @@
+---@class BetterBags: AceAddon
+local addon = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
+---@class Categories: AceModule
+local categories = addon:GetModule('Categories')
+---@class Localization: AceModule
+local L = addon:GetModule('Localization')
+-------------------------------------------------------
+local debug = false
+local function printDebugChatMsg(message)
+	if debug == true then
+		print("[BetterBags Gear Sets] "..message)
+	end
+end
+-------------------------------------------------------
+local function UpdateGearsets() -- Add all items in equipment sets to the "Sets" category
+   local equipmentSetIDs = C_EquipmentSet.GetEquipmentSetIDs()
+
+   for _, i in pairs(equipmentSetIDs) do -- For all sets
+      printDebugChatMsg("Set " .. i)
+      local itemIDs = C_EquipmentSet.GetItemIDs(i)
+      if itemIDs then
+         for _, itemID in pairs(itemIDs) do
+            if itemID and itemID > 0 then
+               printDebugChatMsg("ID: " .. itemID)
+               categories:AddItemToCategory(itemID, "Gear Sets")
+            else
+               printDebugChatMsg("Invalid or missing ItemID \"" .. tostring(itemID) .. "\" in \"" .. i .. "\".")
+            end
+         end
+      else
+         printDebugChatMsg("Missing set \"" .. i .. "\".")
+      end
+   end
+end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("EQUIPMENT_SETS_CHANGED")
+frame:SetScript("OnEvent", function(self, event, ...) -- Update on login and gear change events
+   printDebugChatMsg("Event \"" .. event .. "\" fired.")
+   UpdateGearsets()
+end)
